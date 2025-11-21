355384287ba7ede36e4ee1bdc6d8ff2234797502
lakshmikanth257-cyber <lakshmikanthkanne257@gmail.com>
2025-11-21 18:04:40 +0530

    Add program to convert time to seconds

---
+++ Convert-time-to-seconds.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+void sec(){
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    printf("Total seconds: %d", a*60*60 +b*60 +c);
+    return;
+    
+}
+
+
+int main() {
+    sec();
+  
+    return 0;
+}
