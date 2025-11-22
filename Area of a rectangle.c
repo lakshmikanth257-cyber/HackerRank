63c7b982cb5b76a2ca3e9a2c02ea5755ce0a8e69
lakshmikanth257-cyber <lakshmikanthkanne257@gmail.com>
2025-11-22 13:23:53 +0530

    Add program to calculate area of a rectangle

---
+++ Area of a rectangle.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+void tri(){
+    int a;
+    int b;
+    scanf("%d %d",&a,&b);
+    printf("The area is: %d units" ,a*b);
+    return;
+    
+}
+
+
+int main() {
+    tri();
+
+
+    return 0;
+}
