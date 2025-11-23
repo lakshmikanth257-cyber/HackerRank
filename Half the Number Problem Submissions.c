5973f4ec285b5f065811eb30c2af1731e73a3555
lakshmikanth257-cyber <lakshmikanthkanne257@gmail.com>
2025-11-23 18:36:35 +0530

    Create Half the Number Problem Submissions.c

---
+++ Half the Number Problem Submissions.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+  void hal(){
+     int num;
+      scanf("%d",&num);
+      float half=num/2.00;
+      printf("Half of %d is %.2f",num,half);
+  }
+
+
+
+int main() {
+      
+      hal();
+         
+    return 0;
+}
