e2b57e69bf3be20cf76a910d893308b163cb0598
lakshmikanth257-cyber <lakshmikanthkanne257@gmail.com>
2025-11-23 18:35:37 +0530

    Add program to split a 3-digit number into digits

---
+++ Break a 3 Digit Number into Individual Digits.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+void split(int n)
+{
+    int a=n/100;
+    int b=(n/10)%10;
+    int c=n%10;
+    printf("%d %d %d",a,b,c);
+}
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    split(n);
+    
+    return 0;
+}
