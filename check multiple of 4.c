d6c52a41489993ec8ec8ee9f8e74c11d976964db
lakshmikanth257-cyber <lakshmikanthkanne257@gmail.com>
2025-12-17 18:59:49 +0530

    Create check multiple of 4.c

---
+++ check multiple of 4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+    int num;
+    
+    scanf("%d",&num);
+    
+    if (num%4==0){
+        printf("Yes");
+    }
+    else{
+        printf("No");
+    }
+    
+    
+    
+    return 0;
+}
