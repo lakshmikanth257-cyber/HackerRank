a9c2a042d108284d8ccb3d6fb77252177b190ba1
Kanne Lakshmi Kanth <lakshmikanthkanne257@gmail.com>
2026-01-10 23:36:16 +0530

    Replace negative numbers with zero in array

---
+++ Replace Every Negative Number with 0.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int n;
+    scanf("%d",&n);
+
+    int a[n];
+
+    
+    for (int i=0;i<n;i++){
+        scanf("%d", &a[i]);
+    }    
+    for (int i=0;i<n;i++){
+        if (a[i]<0){
+            a[i]=0;
+        }
+    }
+
+    for (int i=0;i<n;i++) {
+        printf("%d ",a[i]);
+    }
+    return 0;
+}
